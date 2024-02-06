ea19ce7237ab4f63359ae98147f89a0f96c4e16a
Andre Arkhipov <andre.arhipow@gmail.com>
2024-02-06 18:40:18 +0300

    commit

---
+++ src/components/Main/SelectionWagons/TrailDetails.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import GoBackBlock from "../../Molecules/SelectionWagon/GoBackBlock";
+import Card from "../../Main/CardsBlock/Card";
+import { CardBody, CardBottom, CardTop } from "../../Main/CardsBlock/CardsMolecules";
+
+const TrailDetails = ({ className, data }) => {
+  if (!data) {
+    return;
+  }
+
+  return (
+    <React.Fragment>
+      <div className={className + "_details"}>
+        <GoBackBlock className={className} type={" come-back"} />
+        <Card data={data} className={"trains-block"}>
+          <CardTop className={"trains-block_card-top"} data={data} />
+          <CardBody className={"trains-block_body"} data={data} />
+          <CardBottom className={"trains-block"} data={data.duration} />
+        </Card>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default TrailDetails;
